0eae006d182f2d397e0ea7e206b48770cb10db46
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-01-24 19:23:28 -0500

    Clear grid button and random rgb color on hover

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
-const gridContainer = document.querySelector('#grid-container');
-
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('grid-square');
-    
-    gridSquare.addEventListener('mouseenter', event => {
-      event.target.style.backgroundColor = '#000';
-    });
-    
-    gridContainer.appendChild(gridSquare);
-  }
-}
+createGrid(16);
 
 const clearBtn = document.querySelector('#clear-btn');
 
 clearBtn.addEventListener('click', () => {
   const gridSize = prompt('New grid size', 16);
+  clearGrid();
+  createGrid(gridSize);
 });
+
+function createGrid(x) {
+  const gridContainer = document.querySelector('#grid-container');
+  gridContainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
+      const gridSquare = document.createElement('div');
+      gridSquare.classList.add('grid-square');
+      
+      gridSquare.addEventListener('mouseenter', event => {
+        event.target.style.backgroundColor = `rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
+      });
+    
+      gridContainer.appendChild(gridSquare);
+    }
+  }
+}
+
+function clearGrid() {
+  const gridContainer = document.querySelector('#grid-container');
+
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+}
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
